4e6852f763aba012a0e3e53ce32d6ae4238658d3
Hasan Akram <k250098@nu.edu.pk>
2026-02-17 12:04:34 +0500

    Refactor StationeryShop to include Book class

--- question.cpp
+++ question.cpp
@@ -1,93 +1,52 @@
-/******************************************************************************
-
-Design a class for a StationeryShop that maintains a list of all items it sells, stored as an array
-of strings, along with a corresponding list of item prices, stored as an array of doubles. Create
-a menu-driven program that allows the shop owner to perform the following operations: add
-items and their prices, retrieve the list of items, edit item prices, and view all items along with
-their prices. Additionally, implement functionality to generate a receipt that the shopkeeper
-can share with customers. The receipt should be created only after the shopkeeper inputs the
-items purchased by the customer along with their quantities.
-
-*******************************************************************************/
 #include <iostream>
-#include <string.h>
+#include <string>
+
 using namespace std;
 
-class StationeryShop{
-    
-    private:
-    
-    string items[50];
-    double prices[50];
-    
-    int count;
-    
-    
-    
-    public:
-    
-    StationaryShop(){
-        count=0;
+class book {
+private:
+    string title;
+    string author;
+    double price;
+
+public:
+    book() {
+        title = "Untitled";
+        author = "Anonymous";
+        price = 0.0;
     }
-    
-    void setitem(){
-        string a ;
-        int b;
-        cout<<"Enter Item you want to add"<<endl;
-        cin>>a>>endl;
-        cout<<"Enter the price"<<endl;
-        cin>>b>>endl;
-        
-        items[count] = a;
-        prices[count]= b;
-        count ++;
+
+    book(string t, string a = "Unknown", double p = 0.0) {
+        title = t;
+        author = a;
+        price = p;
     }
-    
-    void listitem(){
-        for (int i=0; i<=count; i++){
-            cout<<items[count]<<endl;
-            
-        }
+
+    book(const book &source) {
+        title = source.title;
+        author = source.author;
+        price = source.price;
     }
-    
-    void editprices(){
-        string x;
-        int y;
-        double z;
-        cout<<"enter item you want to change the price of"<<endl;
-        cin>>x>>endl;
-        cout<<"enter item price "<<endl;
-        cin>>z>>endl;
-        
-        for (int i=0; i<= count; i++){
-            if (x == item[count]){
-                y= count;
-            }
-        }
-        prices[y]= z;
+
+    void show_details() const {
+        cout << "Title: " << title 
+             << " | Author: " << author 
+             << " | Price: $" << price << endl;
     }
-    
-    void veiwlist(){
-        for (int i=0; i<=count; i++){
-            cout<<items[count];
-            cout<<" :"<<prices[count]<<endl;
-            
-        }
-    }
-    
-    
-    void buy(){
-        cout<<items
-        
-        
-    }
-    
-    
 };
 
-int main(){
-    
-    
+int main() {
+    book b1;
+    b1.show_details();
+
+    book b2("The Great Gatsby", "F. Scott Fitzgerald", 12.99);
+    b2.show_details();
+
+    book b3("The C++ Chronicles");
+    b3.show_details();
+
+    book b4(b2);
+    b4.show_details();
 
     return 0;
 }
